8f4eca493a2e1b42d7320bc3e5470aedd029e638
iterus <iterus@protonmail.com>
2020-08-18 13:23:21 +0300

    Remove unnecessary dependencies from buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,4 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.0.1")
-    implementation(kotlin("gradle-plugin", version = "1.3.72"))
-    implementation(kotlin("stdlib"))
 }
