883afca4c26377c0a74e7fae5e785290fbed9cfb
iterus <iterus@protonmail.com>
2020-08-18 13:01:26 +0300

    Combine Versions and AndroidConfig

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -6,6 +6,15 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
 import xyz.iterus.build.defaults.dependencies.TestDependencies
 
+private object Versions {
+    const val name = "0.1.0"
+    const val code = 1
+
+    const val compile_sdk = 29
+    const val min_sdk = 19
+    const val target_sdk = 29
+}
+
 internal fun Project.applyAndroidConfig() = extensions.getByType<BaseExtension>().run {
 
     compileSdkVersion(Versions.compile_sdk)
