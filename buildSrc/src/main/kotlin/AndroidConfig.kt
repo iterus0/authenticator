00af9050bfc7a45cea0a819a5ff99b53b8dd4325
iterus <iterus@protonmail.com>
2020-11-05 13:49:20 +0300

    Update compileSdk and targetSdk to api level 30

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -10,9 +10,9 @@ private object Versions {
     const val name = "0.1.0"
     const val code = 1
 
-    const val compile_sdk = 29
+    const val compile_sdk = 30
     const val min_sdk = 19
-    const val target_sdk = 29
+    const val target_sdk = 30
 }
 
 internal fun Project.applyAndroidConfig() = extensions.getByType<BaseExtension>().run {
