9d3ff036e97fff2f569fad8d997572123fb3688f
iterus <iterus@protonmail.com>
2020-11-05 13:37:49 +0300

    Fix double gradle plugin definition

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,13 +14,6 @@ repositories {
     jcenter()
 }
 
-gradlePlugin {
-    plugins.register("build.defaults.android") {
-        id = "build.defaults.android"
-        implementationClass = "xyz.iterus.build.defaults.AndroidDefaultsPlugin"
-    }
-}
-
 dependencies {
     // https://github.com/gradle/kotlin-dsl-samples/issues/1320
     implementation("com.android.tools.build:gradle:4.1.0")
